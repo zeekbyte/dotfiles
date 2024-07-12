e00b9d5d1025a5c9006194aa8ba8b34de7118cb5
zeekbyte <thanbv1510@gmail.com>
2024-07-12 08:44:50 +0700

    add config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add theme
+  { "navarasu/onedark.nvim" },
+
+  -- Configure LazyVim to load theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
---
+++ zsh-install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install zsh
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
